8536d9cade07b20c7d8967ef4670c37b6f780863
Kush Vardhan <kushvardhan39797@gmail.com>
2025-05-01 21:41:21 +0530

    feat: add connection data structure and update Connection component

--- src/components/Connection.jsx
+++ src/components/Connection.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
 
 const Connection = () => {
+  const connections = [
+    { id: 1, name: "John Doe", image: "https://i.imgur.com/6YQ1Zzt.png" },
+    { id: 2, name: "Jane Smith", image: "https://i.imgur.com/6YQ1Zzt.png" },
+    { id: 3, name: "Alice Johnson", image: "https://i.imgur.com/6YQ1Zzt.png" },
+  ];
+
   return (
     <div>Connection</div>
   )
 }
 
-export default Connection
+export default Connection;
