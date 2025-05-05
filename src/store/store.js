d7c6829ae98731c30797dfc041264505e00e790c
Kush Vardhan <kushvardhan39797@gmail.com>
2025-05-05 13:04:29 +0530

    Making changes in store  to prevent  the leakage of UI layer data,

--- src/store/store.js
+++ src/store/store.js
@@ -1,12 +1,26 @@
-import { configureStore } from '@reduxjs/toolkit'
-import userReducer from '../slice/UserSlice';
-import requestReducer from '../slice/RequestSlice';
-import connectionReducer from '../slice/ConnectionSlice';
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import connectionReducer from "../slice/ConnectionSlice";
+import requestReducer from "../slice/RequestSlice";
+import userReducer from "../slice/UserSlice";
+
+// Create a root reducer with a special case for logout
+const appReducer = combineReducers({
+  user: userReducer,
+  request: requestReducer,
+  connection: connectionReducer,
+});
+
+// This root reducer will reset all state when logout action is dispatched
+const rootReducer = (state, action) => {
+  // When logout action is detected, reset all state to initial values
+  if (action.type === "user/logoutUser") {
+    // Reset state to undefined so each reducer returns its initial state
+    state = undefined;
+  }
+
+  return appReducer(state, action);
+};
 
 export const store = configureStore({
-  reducer: {
-    user: userReducer,
-    request: requestReducer,
-    connection: connectionReducer,
-  },
-})
+  reducer: rootReducer,
+});
