ab8faf0ca6b54b9ea53915a79bcf4fad9565152c
Kush Vardhan <kushvardhan39797@gmail.com>
2025-05-13 19:18:32 +0530

    Corrected Variable name issue

--- src/store/store.js
+++ src/store/store.js
@@ -3,7 +3,6 @@ import connectionReducer from "../slice/ConnectionSlice";
 import requestReducer from "../slice/RequestSlice";
 import userReducer from "../slice/UserSlice";
 
-// Create a root reducer with a special case for logout
 const appReducer = combineReducers({
   user: userReducer,
   request: requestReducer,
