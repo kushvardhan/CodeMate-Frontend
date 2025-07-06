b6a529a5270f91eda6d95bef971e6eb9b2b7e1c8
Kush Vardhan <kushvardhan39797@gmail.com>
2025-07-06 21:28:25 +0530

    imported Framer-motion in login page

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,3 +1,4 @@
+import {motion} from 'framer-motion';
 import { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
