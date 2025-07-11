f9dde7670a8a772c2b3ad79760f2cf0061f85a0b
Kush Vardhan <kushvardhan39797@gmail.com>
2025-07-11 19:35:34 +0530

    removed the Clerk and Auth providers

--- src/main.jsx
+++ src/main.jsx
@@ -5,26 +5,13 @@ import { ThemeProvider } from "./context/ThemeContext";
 import App from './App';
 import { store } from './store/store';
 import './index.css';
-import { Auth0Provider } from "@auth0/auth0-react";
-
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
-
-if (!PUBLISHABLE_KEY) {
-  throw new Error('Add your Clerk Publishable Key to the .env file');
-}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Auth0Provider
-      domain={import.meta.env.VITE_AUTH0_DOMAIN}
-      clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
-      authorizationParams={{ redirect_uri: window.location.origin }}
-    >
       <Provider store={store}>
         <ThemeProvider>
           <App />
         </ThemeProvider>
       </Provider>
-    </Auth0Provider>
   </StrictMode>
 );
