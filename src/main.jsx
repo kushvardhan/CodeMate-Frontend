fc8cc947707eccca91acdb54fed422ddb55319bd
Kush Vardhan <kushvardhan39797@gmail.com>
2025-07-11 19:23:39 +0530

    installed Auth0, configuring the Auth0 provider in my main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,30 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { Provider } from "react-redux";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
 import { ThemeProvider } from "./context/ThemeContext";
-import App from "./App";
-import { store } from "./store/store";
-import "./index.css";
-import { ClerkProvider } from '@clerk/clerk-react'
+import App from './App';
+import { store } from './store/store';
+import './index.css';
+import { Auth0Provider } from "@auth0/auth0-react";
 
-
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
 if (!PUBLISHABLE_KEY) {
-  throw new Error('Add your Clerk Publishable Key to the .env file')
+  throw new Error('Add your Clerk Publishable Key to the .env file');
 }
 
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Provider store={store}>
-      <ThemeProvider>
-        <App />
-      </ThemeProvider>
-    </Provider>
+    <Auth0Provider
+      domain={import.meta.env.VITE_AUTH0_DOMAIN}
+      clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
+      authorizationParams={{ redirect_uri: window.location.origin }}
+    >
+      <Provider store={store}>
+        <ThemeProvider>
+          <App />
+        </ThemeProvider>
+      </Provider>
+    </Auth0Provider>
   </StrictMode>
 );
